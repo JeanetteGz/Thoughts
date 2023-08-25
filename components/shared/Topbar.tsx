19967739e4ce13a49cd72c37cf250a15cbf18373
JeanetteGz <jeanetteglz99@gmail.com>
2023-08-25 13:42:39 -0400

    Added Links

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,8 +11,30 @@ function Topbar() {
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Thoughts</p>
       </Link>
 
-      <div classname="flex items-center gap-1">
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
 
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger:
+              "py-2 px-4"
+            }
+          }}
+        />
       </div>
     </nav>
   )
